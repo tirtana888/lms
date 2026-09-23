ff075b07ade921ad8a8a2070cfad551907ca46b1
Raizaaa <89463672+raizasafeel@users.noreply.github.com>
2026-09-23 18:21:24 +0530

    Merge pull request #2780 from raizasafeel/fix/authored-content-write-scope
    fix: perms on assignment

--- lms/lms/doctype/lms_assignment/lms_assignment.py
+++ lms/lms/doctype/lms_assignment/lms_assignment.py
@@ -4,10 +4,11 @@
 import frappe
 from frappe.model.document import Document
 
+from lms.lms.doctype.lms_content_author.lms_content_author import AuthoredDocument
 from lms.lms.schedule_utils import validate_schedule_fields
 from lms.lms.utils import has_course_instructor_role, has_moderator_role
 
 
-class LMSAssignment(Document):
+class LMSAssignment(AuthoredDocument, Document):
 	def validate(self):
 		validate_schedule_fields(self)
